4ee59ae7bfd9d9719cc6e7d24b10fa32dcae2e78
ProFireDev <zakm4@educbe.ca>
2021-12-06 14:56:03 -0700

    add in deploy folder

---
+++ Vue/project-flip/dist/precache-manifest.9a4b53864e3919a63d18057d975ced4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a4a4177d3b10f15b70",
+    "url": "/css/app.73b6963d.css"
+  },
+  {
+    "revision": "feaeca2fc048884205c80290eca93c72",
+    "url": "/img/logo.feaeca2f.png"
+  },
+  {
+    "revision": "2ddc776658f048f8138e9b08e1bd73e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09219083ce29516f8dde",
+    "url": "/js/about.3150c677.js"
+  },
+  {
+    "revision": "c7a4a4177d3b10f15b70",
+    "url": "/js/app.4ff5b184.js"
+  },
+  {
+    "revision": "bf64b34fec22c55dba71",
+    "url": "/js/chunk-vendors.d7ba3b8b.js"
+  },
+  {
+    "revision": "bd030a7b7d5bff76c9adb5ffac26a059",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);

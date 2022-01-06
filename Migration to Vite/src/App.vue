9e0ec960dae11e9d608eceb855c2326fffbbddfb
Pro_fire_devlopment <34112557+ProFireDev@users.noreply.github.com>
2022-01-06 16:36:00 -0700

    update with whatever this was lol

---
+++ Migration to Vite/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="nav">
+        <!--this is a temp nav bar-->
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/Build">Build</router-link> |
+        <router-link to="/contact">Contact</router-link> |
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/shop">Shop</router-link> |
+        <router-link to="/signup">Signup</router-link>
+    </div>
+    <router-view />
+    <!-- This is the router-view
+          if you get an ES lint error message here, its 
+           wrong -->
+    <!-- In vue 3 you can have more then one root element
+           see shorturl.at/sxAEN -->
+</template>

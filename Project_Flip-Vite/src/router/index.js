7cb3c3e2db715ae84e2d0ca0382cd1360427e686
Aakansh-C <81188187+Aakansh-C@users.noreply.github.com>
2022-01-10 22:05:17 -0700

    New Project Flip Vite file
    Only deleted the vite migration file, and replaced with new one

---
+++ Project_Flip-Vite/src/router/index.js
@@ -0,0 +1,61 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '/src/components/Home.vue'
+import About from '/src/components/About.vue'
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about", // this is the path that will be used in the url
+        name: "About", // this is the name of the route
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            // this is the component that will be loaded
+            import(/* webpackChunkName: "about" */ "../views/About.vue"), // this is the path to the component being imported
+    },
+    {
+        path: "/shop",
+        name: "Shop",
+        component: () =>
+            import(/* webpackChunkName: "shop" */ "../views/Shop.vue"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () =>
+            import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+    },
+    {
+        path: "/contact",
+        name: "Contact",
+        component: () =>
+            import(/* webpackChunkName: "contact" */ "../views/Contact.vue"),
+    },
+    {
+        path: "/build",
+        name: "Build",
+        component: () =>
+            import(/* webpackChunkName: "build" */ "../views/Build.vue"),
+    },
+    {
+        path: "/signup",
+        name: "Signup",
+        component: () =>
+            import(/* webpackChunkName: "signup" */ "../views/Signup.vue"),
+    },
+];
+
+const router = createRouter({
+
+    history : createWebHistory(),
+    routes,
+
+})
+
+export default router
---
+++ Project_Flip-Vite/src/views/Shop.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="about">
+        <h1>This is the shop page</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

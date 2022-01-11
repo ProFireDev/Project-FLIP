7cea36c1d5c9f3807d18db0f7d650adfbcc756b1
Aakansh-C <81188187+Aakansh-C@users.noreply.github.com>
2022-01-10 21:58:40 -0700

    Revert "New Vue Ft. Tailwind, Prettier, ESLint, VueX"
    This reverts commit b304c8aa36c9e61434ef9373f26f31313ea986cf.

---
+++ Vue/project-flip/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+        If this is bold and underlined, then it means that Tailwind CSS is
+        working!
+    </h1>
+</template>
+        <style>
+            
+        </style>
+
+    <script>
+
+    </script>
+
+
+

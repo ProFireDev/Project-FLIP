b304c8aa36c9e61434ef9373f26f31313ea986cf
Aakansh-C <81188187+Aakansh-C@users.noreply.github.com>
2022-01-10 21:52:43 -0700

    New Vue Ft. Tailwind, Prettier, ESLint, VueX
    Everything is Working and Stable. No errors.

---
+++ src/views/Build.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="text-3xl font-bold underline">
+        This is the Build Page
+    </h1>
+</template>
+        <style>
+            body {
+                background-color: rgba(0, 0, 0, 0.87);
+                background-repeat: no-repeat;
+                background-position: center;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+            
+        </style>
+
+    <script>
+
+    </script>
+
+

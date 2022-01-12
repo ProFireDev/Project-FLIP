23f8cdd681f35d4478cff4de5534d6f1a42e29f5
ProFireDev <zakm4@educbe.ca>
2022-01-12 15:12:50 -0700

    added start of PWA support

--- Project_Flip-Vite/vite.config.js
+++ Project_Flip-Vite/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue()
-  ]
+    plugins: [
+        vue(),
+        VitePWA({
+            name: 'Project-Flip-vite',
+            short_name: 'Flip-Vite',
+            description: 'Project flip pwa running on vite',
+            background_color: '#ffff',
+            theme_color: '#05a1e4',
+            display: 'standalone',
+            orientation: 'portrait',
+            start_url: '/',
+        }),
+    ],
 })

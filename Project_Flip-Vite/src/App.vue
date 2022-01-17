571e21a904f7ecf64cdcc7ab8f1967f8898d8253
Aakansh-C <81188187+Aakansh-C@users.noreply.github.com>
2022-01-16 20:32:29 -0700

    Jan 16 8:32

--- Project_Flip-Vite/src/App.vue
+++ Project_Flip-Vite/src/App.vue
@@ -8,8 +8,8 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
     <div id="nav_bar" class="flex justify-around items-center text-gray" >
         <!--this is a temp nav bar-->
-        <router-link to="/Home"> <img src="\src\assets\Trans_Home_Page_Logo.png" class="object-scale-down h-48 w-22 flex justify-start hover:underline decoration-orange"> </router-link> 
-       
+        <router-link id="nav_bar_Logo" to="/Home"> <img src="\src\assets\Trans_Home_Page_Logo.png" class="object-left h-48 w-22 scale-125 flex justify-start hover:underline decoration-orange mt-8"> </router-link> 
+
           <router-link to="/shop"  class="font-roboto text-2xl hover:underline decoration-orange decoration-4 underline-offset-4">Shop</router-link> 
           <router-link to="/build" class="font-roboto text-2xl hover:underline decoration-orange decoration-4 underline-offset-4">Build</router-link> 
           <router-link to="/about" class="font-roboto text-2xl hover:underline decoration-orange decoration-4 underline-offset-4">About  </router-link> 
@@ -21,13 +21,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
     </div>
 
-    <div id="social_media">
-        <button onclick="" src="" class=""></button>
-        <button onclick="" src="" class=""></button>
-        <button onclick="" src="" class=""></button>
-        <button onclick="" src="" class=""></button>
-    </div>
-        <h1  class="text-center underline text-3xl font-bold"> Site Is Currently Under Construction</h1>
+
+
+
   
     <router-view />
 
@@ -37,6 +33,7 @@ import HelloWorld from './components/HelloWorld.vue'
 body {
   background-color: rgb(246, 246, 246);
 }
+
 
 </style>
 
--- Project_Flip-Vite/src/views/Home.vue
+++ Project_Flip-Vite/src/views/Home.vue
@@ -1,34 +1,13 @@
-<template>
-
-
-    <div id="filler" class="page_filler" src="\src\assets\Home_Page_Filler.jpg"></div>
-
-
-    <div id="home_sign" class="font-ABeeZee text-6xl text-[#84888C] font-bold tracking-widest pl-[8vw]">
-        <h1> HOME </h1>
-    </div>
-
-
-   <div class="text-center underline text-3xl font-bold">
-        <h1>This is the Home page</h1>
-    </div>
-
-</template>
-
-
-<script>
-
-</script>
-
 <style>
 body  {
     overflow-x: hidden !important;
+    overflow-y: hidden !important;
 }
 
 .page_filler{
 position: absolute;
 
-bottom: -5%;
+bottom: -10%;
 left: -10%;
 
 width: 200%;
@@ -39,16 +18,42 @@ z-index: -1000;
 background: url(/Home_Page_Filler.jpg);
 transform: rotate(-25deg);
 }
-
-/*.page_filler {
-    position: absolute;
-    height: 200%;
-    width: 200%;
-background: url(public/Home_Page_Filler.jpg);
-transform: rotate(-25deg);
-} */
+.socials {
+position: absolute;
+width: 5vw;
+height: 10vh;
+right: 4vw;;
+bottom: 40vh;
+}
 
 </style>
+
+
+<template>
+    <div id="filler" class="page_filler" src="\src\assets\Home_Page_Filler.jpg"></div>
+
+
+  <div id="home_sign" class="font-ABeeZee text-7xl text-[#84888C] font-bold tracking-widest pl-[7vw] pt-[4.5vh]">
+        <h1> HOME </h1>
+    </div>
+    <h1 class="font-ABeeZee text-[#c8cbce] text-3xl pl-[7vw] pt-[4vh] italic"> We strive for the best <br> experience for our customers </h1>
+
+
+    <div id="socials" class="socials flex flex-wrap scale-125"> 
+       <a href="https://discord.gg/TrMaZxY47j"><img class="p-[2vh] scale-125" src="\src\assets\Discord_Logo.png"></a>
+        <a href="https://www.instagram.com/flipping_industries/?hl=en"><img class="p-[2vh]" src="\src\assets\Instagram_Logo.png"></a>
+        <a href="https://twitter.com/indistrys?t=onBAOqdrYkKGmb01DXh3Zw&s=09"><img class="p-[2vh]" src="\src\assets\Twitter_Logo.png"></a>
+        <a><img class="p-[2vh]" src="\src\assets\Facebook_Logo.png"></a>
+       
+       </div>
+https://discord.gg/TrMaZxY47j
+</template>
+
+
+<script>
+
+</script>
+
 
 
 <!--figure out a proper nav bar and footer here first, then get the rest of the content together-->

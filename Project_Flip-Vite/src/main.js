e6fb18201a24188509a18bad98cf3c5b9279c0d7
ProFireDev <zakm4@educbe.ca>
2022-01-17 14:20:30 -0700

    remove last issue with backend

--- Project_Flip-Vite/src/main.js
+++ Project_Flip-Vite/src/main.js
@@ -7,9 +7,3 @@ import store from './store'
 import './index.css'
 
 createApp(App).use(router).use(store).mount('#app')
-
-Vue.use(VeeValidate)
-
-// setup fake backend
-import { configureFakeBackend } from './backend/accounts/_helpers/helper_export.js'
-configureFakeBackend()
